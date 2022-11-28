06d500b56d0f5277282f7de0c761976ae7d3abd5
kalina235 <46924011+kalina235@users.noreply.github.com>
2022-11-28 00:04:16 +0000

    Update access.py
    dumping basic functions from notebook to access

--- fynesse/access.py
+++ fynesse/access.py
@@ -15,3 +15,67 @@ def data():
     """Read the data from the web or local file, returning structured format such as a data frame"""
     raise NotImplementedError
 
+def execute(cur, sqlquery =""):
+    cur.execute(sqlquery)
+    return cur.fetchall()
+    
+def create_pp_data():
+  cur.execute(f'''
+  DROP TABLE IF EXISTS `pp_data`;''')
+  cur.execute(f'''
+  CREATE TABLE IF NOT EXISTS `pp_data` (
+    `transaction_unique_identifier` tinytext COLLATE utf8_bin NOT NULL,
+    `price` int(10) unsigned NOT NULL,
+    `date_of_transfer` date NOT NULL,
+    `postcode` varchar(8) COLLATE utf8_bin NOT NULL,
+    `property_type` varchar(1) COLLATE utf8_bin NOT NULL,
+    `new_build_flag` varchar(1) COLLATE utf8_bin NOT NULL,
+    `tenure_type` varchar(1) COLLATE utf8_bin NOT NULL,
+    `primary_addressable_object_name` tinytext COLLATE utf8_bin NOT NULL,
+    `secondary_addressable_object_name` tinytext COLLATE utf8_bin NOT NULL,
+    `street` tinytext COLLATE utf8_bin NOT NULL,
+    `locality` tinytext COLLATE utf8_bin NOT NULL,
+    `town_city` tinytext COLLATE utf8_bin NOT NULL,
+    `district` tinytext COLLATE utf8_bin NOT NULL,
+    `county` tinytext COLLATE utf8_bin NOT NULL,
+    `ppd_category_type` varchar(2) COLLATE utf8_bin NOT NULL,
+    `record_status` varchar(2) COLLATE utf8_bin NOT NULL,
+    `db_id` bigint(20) unsigned NOT NULL
+  ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;''')
+  return cur.fetchall()
+
+def download_prop_data(beg_year=1995, end_year=2022):
+  for year in range(beg_year, end_year+1):
+    urllib.request.urlretrieve(f'http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-{year}.csv', f'pp_data{year}.csv')
+   
+def upload_prop_data(beg_year=1995, end_year=2022):
+  for year in range(beg_year, end_year+1):
+    print(year)
+    cur.execute(f'''
+    LOAD DATA LOCAL INFILE 'pp_data{year}.csv' INTO TABLE pp_data
+    FIELDS TERMINATED BY ',' 
+    ENCLOSED BY '"';
+    ''')
+  
+def select_top(conn, table,  n):
+    """
+    Query n first rows of the table
+    :param conn: the Connection object
+    :param table: The table to query
+    :param n: Number of rows to query
+    """
+   # cur = conn.cursor()
+    cur.execute(f'SELECT * FROM {table} LIMIT {n}')
+
+    rows = cur.fetchall()
+    return rows
+
+def head(conn, table, n=5):
+  rows = select_top(conn, table, n)
+  for r in rows:
+      print(r)
+    
+#def data_info(tablename = "pp_data", ):
+    
+   
+
